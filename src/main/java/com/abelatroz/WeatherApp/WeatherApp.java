54fc407be080a8fe982f2e89868ff17e024887a8
Amanda Cardozo Soares Castilho <filipe.bmorais@hotmail.com>
2024-07-21 15:34:03 -0300

    Inicio do projeto WeatherApp

--- src/main/java/com/abelatroz/WeatherApp/WeatherApp.java
+++ src/main/java/com/abelatroz/WeatherApp/WeatherApp.java
@@ -1,10 +1,24 @@
 package com.abelatroz.WeatherApp;
 
+import java.net.URL;
+import java.util.Scanner;
+
 public class WeatherApp {
+	
+	private static final String API_KEY = "365ba3b15a813aa62f189fcc4daa3d84"; // Substitua com sua chave da API
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		
+		System.out.println("Digite o nome da cidade: ");
+		String city = scan.next();
+		
+		scan.close();
+		
+		
+		String apiUrl = String.format("http://api.openweathermap.org/data/2.5/forecast?q%s&appid=%s", city, API_KEY);
+		URL url = new URL(apiUrl);
 	}
 
 }
