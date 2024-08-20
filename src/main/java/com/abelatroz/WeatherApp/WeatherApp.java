f40fef1e8196bf913c869b2158a6efaecd473e84
Filipe Brito <filipe.bmorais@hotmail.com>
2024-08-19 22:01:44 -0300

    Tratamento do retorno da API

--- src/main/java/com/abelatroz/WeatherApp/WeatherApp.java
+++ src/main/java/com/abelatroz/WeatherApp/WeatherApp.java
@@ -3,7 +3,11 @@ package com.abelatroz.WeatherApp;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class WeatherApp {
 	
@@ -28,14 +32,27 @@ public class WeatherApp {
 			int responseCode = connection.getResponseCode();
 			System.out.println("código da resposta: " + responseCode);
 			if(responseCode == 200) {
-				Scanner responseScanner = new Scanner(connection.getInputStream());
-				StringBuilder response = new StringBuilder();
-				while(responseScanner.hasNext()){
-					response.append(responseScanner.nextLine());
-				}
-				responseScanner.close();
-				
-				System.out.println("Resposta da API: " + response.toString());
+			    Scanner responseScanner = new Scanner(connection.getInputStream());
+			    StringBuilder response = new StringBuilder();
+			    while (responseScanner.hasNext()) {
+			        response.append(responseScanner.nextLine());
+			    }
+			    responseScanner.close();
+			    
+			    JSONObject jsonResponse = new JSONObject(response.toString());
+			    JSONArray weatherList = jsonResponse.getJSONArray("list");
+			    JSONObject firstWeatherEntry = weatherList.getJSONObject(0);
+			    
+			    // Obtendo a data e hora da previsão
+			    long timestamp = firstWeatherEntry.getLong("dt");
+			    Date date = new Date(timestamp * 1000); // Converter para milissegundos
+			    System.out.println("Data e hora da previsão: " + date.toString());
+
+			    JSONObject main = firstWeatherEntry.getJSONObject("main");
+			    double temperatureInKelvin = main.getDouble("temp");
+			    double temperatureInCelsius = temperatureInKelvin - 273.15;
+			    
+			    System.out.printf("Temperatura: %.2f°C\n", temperatureInCelsius);
 			}else {
 				System.out.println("Erro na requisição: " + responseCode);
 			}
